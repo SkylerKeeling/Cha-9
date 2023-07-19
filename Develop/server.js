382bc4f3aff174b1402047955a2ff0774f103b93
skyler <skyler@skylers-MacBook-Pro.local>
2023-07-19 16:14:28 -0300

    HTMLRoutes developped

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const HTMLRoutes = require("./HTMLRoutes/HTMLRoutes.js")
+const APIRoutes = require("./APIRoutes/APIRoutes.js")
+const app = express()
+const port = 3001
+//route middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+app.use("/", HTMLRoutes)
+app.use("/api", APIRoutes)
+app.listen(port, function () {
+  console.log(`app listening at https://localhost:${port}`)
+})
